2d017db4bd8e38ccd8d44b27d15adb06c6f51378
Joe <overtonje@gmail.com>
2021-03-12 13:45:45 -0600

    adding rnadomword

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -4,6 +4,12 @@ defmodule Dictionary do
   end
 
   def random_word() do
-    IO.puts("random")
+    Enum.random(word_list())
+    # IO.puts("random")
+  end
+
+  def word_list do
+    contents = File.read!("assets/words.txt")
+    list = String.split(contents, ~r/\n/)
   end
 end
