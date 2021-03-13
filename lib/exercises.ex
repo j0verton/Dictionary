86465f5638e8f35b019ece23521fd286cb562aa5
Joe <overtonje@gmail.com>
2021-03-13 17:42:37 -0600

    finished exercises

--- lib/exercises.ex
+++ lib/exercises.ex
@@ -12,17 +12,29 @@ defmodule Exercises do
 
 
 
+
+
+
+
+
   def reverse_string do
     time = "had we but world enough, and time"
     String.reverse(time)
 
   end
 
-  bacon = "“had we but bacon enough, and treacle”"
 
   def split_at_comma do
     time = "had we but world enough, and time"
     String.split(time, ",")
 
   end
+
+  def difference do
+    time = "had we but world enough, and time"
+    bacon = "had we but bacon enough, and treacle"
+
+    String.myers_difference(time, bacon)
+  end
+
 end
