15f1625b5f7ccf22ab52aff7a6aa0566521d32c9
Joe <overtonje@gmail.com>
2021-03-13 17:07:28 -0600

    working on exercises

---
+++ lib/exercises.ex
@@ -0,0 +1,26 @@
+defmodule Exercises do
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  bacon = "“had we but bacon enough, and treacle”"
+
+  def split_at_comma do
+    time = "had we but world enough, and time"
+    String.split(time, ",")
+
+  end
+end
